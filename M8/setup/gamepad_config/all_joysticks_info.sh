d61f8c78eeadab03398714e478d12acf67843198
smootalicious <smootalicious@gmail.com>
2022-06-28 18:00:23 -0500

    Echo gamepad_config info if running from ssh

--- M8/setup/gamepad_config/all_joysticks_info.sh
+++ M8/setup/gamepad_config/all_joysticks_info.sh
@@ -4,6 +4,6 @@ cd $CURRENT_PATH
 
 sudo chmod 666 /dev/tty1;
 echo -e "\e[032m" > /dev/tty1;
-echo "Running gamepad_config on device...";
+echo "Running './gamepad_config -l' on device...";
 ./gamepad_config -l > /dev/tty1;
  printf "\033c" >> /dev/tty1;
--- M8/setup/gamepad_config/test_gamepad_1.sh
+++ M8/setup/gamepad_config/test_gamepad_1.sh
@@ -3,6 +3,6 @@ CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CURRENT_PATH
 
 sudo chmod 666 /dev/tty1;
-echo "Running gamepad_config on device...";
+echo "Running './gamepad_config -g 1' on device...";
 ./gamepad_config -g 1 > /dev/tty1;
 printf "\033c" >> /dev/tty1;
