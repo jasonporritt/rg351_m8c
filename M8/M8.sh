6f8b94fc2445f3d7c13ec79073d844fa449fa884
smootalicious <107370919+smootalicious@users.noreply.github.com>
2022-07-09 15:23:55 -0500

    Add alsaloop_wait to loopback after m8c starts  in case wait_for_device=true or other failure.

--- M8/M8.sh
+++ M8/M8.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Uncomment the following lines to disable wi-fi
+#sudo modprobe -r mt7601u
+#sudo sed -i '$ablacklist mt7601u' /etc/modprobe.d/blacklist.conf
+#printf "\n\n\n\e[32mWifi has been disabled.\n"
+
 m8_path=ports/M8
 roms2_path=/roms2/$m8_path
 roms_path=/roms/$m8_path
@@ -8,7 +15,16 @@ else
   cd $roms_path
 fi
 
-alsaloop -P hw:0,0 -C hw:1,0 -t 50000 -B 5000 --rate 44100 --sync=0 -d
+
+# alsaloop_wait will run in background and try to create the loopback 
+# if it can't be created before m8c runs. Useful for wait_for_device=true. 
+./alsaloop_wait &
+#alsaloop -P hw:0,0 -C hw:1,0 -t 200000 -A 5 --rate 44100 --sync=0 -T -1 -d
 ./_m8c/m8c
 
 pkill alsaloop
+
+# Uncomment the following lines to enable wi-fi
+#sudo modprobe -i mt7601u
+#sudo sed -i '/blacklist mt7601u/d' /etc/modprobe.d/blacklist.conf
+#printf "\n\n\n\e[32mWifi has been enabled.\n"
